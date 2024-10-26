3b26f804afa211d5e350fc64ad837382f43332bb
“iteweb” <suphakrit.panther@gmail.com>
2024-10-27 04:14:08 +0700

    Update

--- api/server.js
+++ api/server.js
@@ -81,6 +81,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.listen(3000, () => {
+    console.log('App listening on port 3000');
+});
+
 app.get('/', (req, res) => {
     console.log('GET /'); // Log when root endpoint is accessed
     res.json({ message: 'Hello World!' });
