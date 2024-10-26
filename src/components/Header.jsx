fe5a2881bb991806f8cad28ef2f9a5af500e39fc
“iteweb” <suphakrit.panther@gmail.com>
2024-10-26 07:16:59 +0700

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className="header">
             <img style={{ width: "400px", height: "150px", padding: "0px"}} src={droneImage} alt="drone image" />
-            <h1 style={{ fontWeight: "400", fontSize: "48px" }}>➢ DRONE COFIGURATION ✯</h1>
+            <h1 style={{ fontWeight: "400", fontSize: "48px" }}>➢ DRONE CONFIGURATION ✯</h1>
             {/* <img style={{ width: "90px", height: "90px", padding: "50px"}} src="https://assets.goal.com/images/v3/blt4c331b3283695ba6/Man_Utd_Rating.jpg" alt="drone image" /> */}
         </div>
     )
